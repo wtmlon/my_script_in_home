5635c61571bc2df556155915488210ad75095d53
liuting <wtmlon@foxmail.com>
2021-07-13 20:13:51 -0700

    #feature vim7.4安装

--- upgrade_vim.sh
+++ upgrade_vim.sh
@@ -1,7 +1,13 @@
-git clone https://github.com/vim/vim.git
-cd vim
-./configure  --enable-pythoninterp=yes
-make -j 8
+#git clone https://github.com/vim/vim.git
+#cd vim
+#./configure  --enable-pythoninterp=yes
+#make -j 8
+#make install
+#cp src/vim /usr/bin
+#vim --version
+wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
+tar jxvf vim-7.4.tar.bz2
+cd vim74/src
+./configure
+make
 make install
-cp src/vim /usr/bin
-vim --version
